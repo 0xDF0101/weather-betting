3171bc2efe60dc2543d06938da4c8f9ef05ec4ad
EugeneTA <0xdf0101@gmail.com>
2026-06-09 16:16:22 +0900

    init

---
+++ src/main/java/com/project/weatherbetting/WeatherBettingApplication.java
@@ -0,0 +1,13 @@
+package com.project.weatherbetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherBettingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherBettingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/weatherbetting/WeatherBettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.weatherbetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherBettingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
